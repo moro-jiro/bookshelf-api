a83d4307674e5e32f6adc4c55a57de5c272a75fe
moro <morojiro@gmail.com>
2024-08-17 06:12:41 +0900

    テストの修正

--- src/test/kotlin/com/example/bookshelf/bookshelf_api/BookshelfApiApplicationTests.kt
+++ src/test/kotlin/com/example/bookshelf/bookshelf_api/BookshelfApiApplicationTests.kt
@@ -1,9 +1,10 @@
 package com.example.bookshelf.bookshelf_api
 
+import com.example.bookshelf_api.BookshelfApiApplication
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(classes = [BookshelfApiApplication::class])
 class BookshelfApiApplicationTests {
 
 	@Test
